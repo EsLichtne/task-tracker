65b88f148250c523b490d275113572fb6c20047e
Ксения <easyghoul@gmail.com>
2024-04-10 14:21:20 +0700

    feat: Добавляет моковые данные

---
+++ mocks.js
@@ -0,0 +1,29 @@
+const tasks = [
+  {
+    id: 'task-1',
+    text: 'Выгулять собаку',
+    checked: false,
+  },
+  {
+    id: 'task-2',
+    text: 'Дочитать книгу',
+    checked: false,
+  },
+  {
+    id: 'task-3',
+    text: 'Отправить письмо маме',
+    checked: false,
+  },
+  {
+    id: 'task-4',
+    text: 'Сделать зарядку',
+    checked: true,
+  },
+  {
+    id: 'task-5',
+    text: 'Позвонить риелтору',
+    checked: false,
+  },
+];
+
+export { tasks };
